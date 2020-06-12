37744fe07d00194e3502ef28b1488a5e8f18c6a2
dimitri <dimitri@trizic.com>
2020-06-12 10:33:30 -0700

    spring boot skeleton

---
+++ src/main/java/com/tecacet/money/JavaMoneySpringPocApplication.java
@@ -0,0 +1,13 @@
+package com.tecacet.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMoneySpringPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMoneySpringPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tecacet/money/JavaMoneySpringPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecacet.money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMoneySpringPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
